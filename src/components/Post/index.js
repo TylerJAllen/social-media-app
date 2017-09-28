a82b358d211515cf594dd2e0c5ea0b5494249be9
Tyler Allen <mynameistylerallen@gmail.com>
2017-09-28 08:28:24 -0700

    add some styling to Post.

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -8,18 +8,22 @@ class Post extends React.Component {
 
   render(){
     return(
-      <div className="post">
-        <div className="post-image">
-        <p>image</p>
+      <div className="post flex-row">
+        <div className="image-container">
+          <div className="post-image">
+            <p>image</p>
+          </div>
         </div>
-        <div className="post-header">
-        <p>header</p>
-        </div>
-        <div className="post-content">
-        <p>content</p>
-        </div>
-        <div className="post-footer">
-        <p>footer</p>
+        <div className="text-container">
+          <div className="post-header">
+            <p>header</p>
+          </div>
+          <div className="post-content">
+            <p>content</p>
+          </div>
+          <div className="post-footer">
+            <p>footer</p>
+          </div>
         </div>
       </div>
     );
