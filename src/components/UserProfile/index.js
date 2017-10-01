3c292584a80ae1abdb7e308518f173a21bdcfeeb
Tyler Allen <Tyler.Allen@akqa.com>
2017-09-30 17:40:47 -0700

    create UserProfile component.

---
+++ src/components/UserProfile/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+class UserProfile extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      <div className="user-profile">
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
